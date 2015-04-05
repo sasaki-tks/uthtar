206017387dab880f420d896121c4b3159c2ceaca
kenta <seasidemanblues@gmail.com>
2015-04-05 16:27:48 +0000

    test5

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
   get 'static_pages/help'
 
   get 'static_pages/about'
-  # The priority is based upon order of creation: first created -> highest priority.
+  
+  get 'static_pages/contact'
+
+ # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
