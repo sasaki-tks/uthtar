3019e1b7e058212f1df646acb58886c7851e86f9
kenta <seasidemanblues@gmail.com>
2015-04-05 16:01:52 +0000

    finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,8 +7,10 @@ describe "StaticPages" do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
-
-
+   end
+    it "should have the right title" do
+  visit '/static_pages/home'
+  expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
  end
   end
  describe "Help page" do
@@ -17,7 +19,11 @@ describe "StaticPages" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
-  end
+it "should have the right title" do
+  visit '/static_pages/help'
+  expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+end  
+end
 
   describe "About page" do
 
@@ -25,6 +31,10 @@ describe "StaticPages" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
     end
+ it "should have the right title" do
+  visit '/static_pages/about'
+  expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
+end
   end
 
 end
